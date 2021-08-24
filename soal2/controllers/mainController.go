78e3a8ddaf7a4f13ed7a9b39542ed9c31c87dedf
Alvin Lesmana <alvin.lesmana88@gmail.com>
2021-08-24 17:47:11 +0700

    handle data not found

--- soal2/controllers/mainController.go
+++ soal2/controllers/mainController.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"net/http"
 	"soal2/helper"
+	"soal2/services"
 	"strings"
 
 	"github.com/labstack/echo/v4"
@@ -11,7 +12,7 @@ import (
 func Search(c echo.Context) error {
 	searchword := c.QueryParam("searchword")
 	pagination := c.QueryParam("pagination")
-	resp := helper.Search(searchword, pagination)
+	resp := services.Search(searchword, pagination)
 	if len(resp.Search) == 0 {
 		respErr := helper.ResponseError("Data not found !")
 		return c.JSON(http.StatusOK, respErr)
@@ -22,7 +23,11 @@ func Search(c echo.Context) error {
 
 func DetailID(c echo.Context) error {
 	idMovie := c.QueryParam("idMovie")
-	resp := helper.DetailMovieByID(idMovie)
+	resp := services.DetailMovieByID(idMovie)
+	if resp.Actors == "" {
+		respErr := helper.ResponseError("Data not found !")
+		return c.JSON(http.StatusOK, respErr)
+	}
 	respSucc := helper.ResponseSuccess("Data "+idMovie+" fetch successfully", resp)
 	return c.JSON(http.StatusOK, respSucc)
 }
@@ -30,7 +35,11 @@ func DetailID(c echo.Context) error {
 func DetailTitle(c echo.Context) error {
 	titleMovieParam := c.QueryParam("titleMovie")
 	titleMovie := strings.ReplaceAll(titleMovieParam, " ", "+")
-	resp := helper.DetailMovieByTitle(titleMovie)
+	resp := services.DetailMovieByTitle(titleMovie)
+	if resp.Actors == "" {
+		respErr := helper.ResponseError("Data not found !")
+		return c.JSON(http.StatusOK, respErr)
+	}
 	respSucc := helper.ResponseSuccess("Data "+titleMovieParam+" fetch successfully", resp)
 	return c.JSON(http.StatusOK, respSucc)
 }
