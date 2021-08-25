2ed8e9c7584a0f49118d7a4ac47773c443d5b314
Alvin Lesmana <alvin.lesmana88@gmail.com>
2021-08-25 14:24:29 +0700

    update soal 1

--- soal1.sql
+++ soal1.sql
@@ -1,16 +1,16 @@
--- create
+-- CREATE
 CREATE TABLE USER (
   ID INTEGER PRIMARY KEY,
   UserName TEXT NOT NULL,
   Parent INTEGER NULL
 );
 
--- insert
+-- INSERT
 INSERT INTO USER VALUES (1, 'Ali', 2);
 INSERT INTO USER VALUES (2, 'Budi', 0);
 INSERT INTO USER VALUES (3, 'Cecep', 1);
 
--- fetch 
+-- QUERY 
 SELECT u1.ID, u1.UserName, u2.UserName 
 FROM USER u1
 LEFT JOIN USER u2
